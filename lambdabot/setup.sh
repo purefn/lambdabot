ec08d5b4e4e2a57c1fa021c063bf0a52ac55114f
Richard Wallace <rwallace@thewallacepack.net>
2014-09-18 09:37:10 -0700

    update setup script to drop oeis

--- lambdabot/setup.sh
+++ lambdabot/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cabal sandbox init
-cabal install djinn conduit-1.1.7 ../oeis ../pontarius-xmpp ../lambdabot*
+cabal install djinn conduit-1.1.7 ../pontarius-xmpp ../lambdabot*
 cabal exec hoogle data
 
